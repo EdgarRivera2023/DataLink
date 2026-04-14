5dd79ec5a72fbf4978011aca9bbe7e0f4ee7a981
Edgar Rivera <xtintores@gmail.com>
2026-04-13 20:12:38 -0400

    Removing middleware to fix 500 error

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,9 @@
-//import { withAuth } from "next-auth/middleware"
+import { NextResponse } from 'next/server'
 
-//export default withAuth({
-  //callbacks: {
-    //authorized: ({ token }) => !!token,
-  //},
-//})
+export default function middleware(request) {
+  return NextResponse.next()
+}
 
-//export const config = {
-  //matcher: ["/dashboard/:path*"],
-//}
+export const config = {
+  matcher: [], // No aplica a nada
+}
